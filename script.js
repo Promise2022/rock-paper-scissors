8c2070109f88d4ecc26bc0992d83b591e02d3f55
Promise2022 <promiseramphadi@gmail.com>
2022-06-02 02:15:55 +0200

    CREATE FUNCTION playRound
    Create a function named playRound that plays a single round of the game and returns a string that declare a winner between player and computer

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const plays = ["Rock", "Paper", "Scissors"]
+let playerSelection = "Rock"
+let computerSelection = computerPlay()
 
 function computerPlay() {
-let randomIndex = Math.floor(Math.random() * 3 )
-return plays[randomIndex]
+    let randomIndex = Math.floor(Math.random() * 3 )
+    return plays[randomIndex]
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection)
+    console.log(computerSelection)
+
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors"
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win! Paper beats rock"
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You lose! Scissors beat Paper"
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You Lose! Rock beats Scissors"
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Win! Scissors beat Paper"
+    } else {
+        return "It's a Tie game!"
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection))
+
+
